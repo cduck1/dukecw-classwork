2c8ef3ba37e26862aed702eaad84f5da3d41cfd4
cduck1 <chrisduke62@gmail.com>
2020-11-03 10:18:06 +0000

    Delete WSTask_4_part_1.py

--- WSTask_4_part_1.py
+++
@@ -1,6 +0,0 @@
-import pdb
-students = int(input("Enter the number of students: "))
-books = int(input("Enter the number of books: "))
-distribution = books//students
-remainder = books % students 
-print("Every student will have",distribution,"books each and there will be",remainder," books left.")
